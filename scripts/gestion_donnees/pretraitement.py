c261ee6333200aa1a2702c7a876687ccc66d556a
Mazen BENHMIDA <mazen.benhmida@esprit.tn>
2023-11-06 18:40:21 -0500

    second commit : Class Pretraitement

--- scripts/gestion_donnees/pretraitement.py
+++ scripts/gestion_donnees/pretraitement.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from scipy import stats
 import umap.umap_ as umap
+from sklearn.decomposition import PCA
 
 class Pretraitement :
 
@@ -30,30 +31,7 @@ class Pretraitement :
 
         x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, random_state=1)
         return x_train, x_test, y_train, y_test,x_val, y_val
-
-
-    def Normaliser(self,df : pd.DataFrame) :
-        scaler=preprocessing.MinMaxScaler()
-        df1=scaler.fit_transform(df)
-        final = pd.DataFrame(df1,df.columns)
-        return final
-
-    def Normalize_dataframe(self,df : pd.DataFrame):
-        col_num = df.select_dtypes(include=[np.number]).columns.tolist()
-        df_normalizer = df.copy()
-        scaler = preprocessing.MinMaxScaler()
-        df_normalizer[col_num] = scaler.fit_transform(df_normalizer[col_num])
-
-        return df_normalizer
-
-
- 
-    def Umap(self, data : pd.DataFrame, n_components : int):
-
-        umap = umap(n_components=n_components)
-        reduced_data = umap.fit_transform(data)
-        return reduced_data
-
+        
 
     def Indice_outliers(self, df :pd.DataFrame,seuil : float) :
         z_scores=stats.zscore(df)
@@ -67,4 +45,25 @@ class Pretraitement :
                 outliers.append(i)
         
         return outliers
+
+
+    # def Nombre_comp_optimal(df:pd.DataFrame , max_n_components):
+    #     variance_expliqué = []
+        
+    #     for n in range(1, max_n_components + 1):
+    #         pca = PCA(n_components=n)
+    #         X_pca = pca.fit_transform(df)
+    #         embedding = umap(n_components=n).fit_transform(X_pca)
+    #         variance_expliqué.append(np.sum(pca.explained_variance_ratio_))
+    #     best_n_components = np.argmax(np.diff(variance_expliqué) <= 0) + 1
+        
+    #     return best_n_components
+ 
+    # def Umap(self, data : pd.DataFrame, n_components : int):
+
+    #     umap = umap(n_components=n_components)
+    #     reduced_data = umap.fit_transform(data)
+    #     return reduced_data
+
+
 
