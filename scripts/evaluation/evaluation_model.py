9ed282aa23a7ed8229a7b7ece55f23df2d67c627
Mazen BENHMIDA <mazen.benhmida@esprit.tn>
2023-12-08 23:46:31 -0500

    commit : correction de quelques classes

--- scripts/evaluation/evaluation_model.py
+++ scripts/evaluation/evaluation_model.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import roc_curve, auc, confusion_matrix, classification_report, f1_score, precision_score, recall_score
 from sklearn.preprocessing import LabelBinarizer
-
+from sklearn.preprocessing import label_binarize
 class Evaluation(object):
     def __init__(self, model, x_train, y_train, x_test, y_test):
         self.model = model
@@ -64,48 +64,84 @@ class Evaluation(object):
         report = classification_report(self.y_test, y_pred)
         print("Rapport de classification :\n", report)
 
-    def plot_learning_curves(self, train_sizes, scoring='accuracy'):
-        # Fonctions pour tracee les courbes d'apprentissage pour evaluer la performance du modele
-        train_sizes, train_scores, val_scores = learning_curve(
+    def plot_learning_curves_loss(self, train_sizes):
+        train_sizes, train_scores, test_scores = learning_curve(
             self.model, self.x_train, self.y_train, cv=5,
-            train_sizes=train_sizes, scoring=scoring)
-        
-        train_mean = np.mean(train_scores, axis=1)
-        train_std = np.std(train_scores, axis=1)
-        val_mean = np.mean(val_scores, axis=1)
-        val_std = np.std(val_scores, axis=1)
+            train_sizes=train_sizes, scoring='accuracy')
+
+        # Calcul de la perte comme 1 - accuracy
+        train_loss = 1 - np.mean(train_scores, axis=1)
+        train_loss_std = np.std(train_scores, axis=1)
+        test_loss = 1 - np.mean(test_scores, axis=1)
+        test_loss_std = np.std(test_scores, axis=1)
 
         plt.figure(figsize=(12, 8))
-        plt.plot(train_sizes, train_mean, label='Score d\'entraînement')
-        plt.fill_between(train_sizes, train_mean - train_std, train_mean + train_std, alpha=0.1)
-        plt.plot(train_sizes, val_mean, label='Score de validation')
-        plt.fill_between(train_sizes, val_mean - val_std, val_mean + val_std, alpha=0.1)
-        plt.title('Courbes d\'apprentissage')
+        plt.plot(train_sizes, train_loss, 'o-', color="r", label="Perte d'entraînement")
+        plt.fill_between(train_sizes, train_loss - train_loss_std, train_loss + train_loss_std, alpha=0.1, color="r")
+        plt.plot(train_sizes, test_loss, 'o-', color="g", label="Perte de test")
+        plt.fill_between(train_sizes, test_loss - test_loss_std, test_loss + test_loss_std, alpha=0.1, color="g")
+
+        plt.title('Courbes de perte d\'apprentissage')
         plt.xlabel('Taille de l\'ensemble d\'entraînement')
-        plt.ylabel(scoring.capitalize())
+        plt.ylabel('Perte')
+        plt.legend(loc='best')
+        plt.grid()
+        plt.show()
+    
+    def plot_learning_curves_accuracy(self, train_sizes):
+        train_sizes, train_scores, test_scores = learning_curve(
+            self.model, self.x_train, self.y_train, cv=5,
+            train_sizes=train_sizes, scoring='accuracy')
+
+        # Calcul de la moyenne et de l'écart-type des scores
+        train_mean = np.mean(train_scores, axis=1)
+        train_std = np.std(train_scores, axis=1)
+        test_mean = np.mean(test_scores, axis=1)
+        test_std = np.std(test_scores, axis=1)
+
+        plt.figure(figsize=(12, 8))
+        plt.plot(train_sizes, train_mean, 'o-', color="r", label="Précision d'entraînement")
+        plt.fill_between(train_sizes, train_mean - train_std, train_mean + train_std, alpha=0.1, color="r")
+        plt.plot(train_sizes, test_mean, 'o-', color="g", label="Précision de test")
+        plt.fill_between(train_sizes, test_mean - test_std, test_mean + test_std, alpha=0.1, color="g")
+
+        plt.title('Courbes d\'apprentissage de précision')
+        plt.xlabel('Taille de l\'ensemble d\'entraînement')
+        plt.ylabel('Précision')
         plt.legend(loc='best')
         plt.grid()
         plt.show()
 
-    def plot_learning_curves_loss(self, train_sizes, scoring='neg_log_loss'):
-        # Fonction pour tracer les courbes d'apprentissage de la perte pour evaluer la performance du modele
-        train_sizes, train_scores, val_scores = learning_curve(
-            self.model, self.x_train, self.y_train, cv=5,
-            train_sizes=train_sizes, scoring=scoring, shuffle=True, random_state=42)
 
-        train_mean = np.mean(train_scores, axis=1)
-        train_std = np.std(train_scores, axis=1)
-        val_mean = np.mean(val_scores, axis=1)
-        val_std = np.std(val_scores, axis=1)
+    def tracer_courbes_roc(classifieurs, y_test):
+       
+        y_test_binarise = label_binarize(y_test, classes=np.unique(y_test))
+        n_classes = y_test_binarise.shape[1]
 
-        plt.figure(figsize=(12, 8))
-        plt.plot(train_sizes, train_mean, label='Perte d\'entraînement')
-        plt.fill_between(train_sizes, train_mean - train_std, train_mean + train_std, alpha=0.1)
-        plt.plot(train_sizes, val_mean, label='Perte de validation')
-        plt.fill_between(train_sizes, val_mean - val_std, val_mean + val_std, alpha=0.1)
-        plt.title('Courbes d\'apprentissage - Perte')
-        plt.xlabel('Taille de l\'ensemble d\'entraînement')
-        plt.ylabel('Perte')
-        plt.legend(loc='best')
+        plt.figure(figsize=(10, 8))
+
+        for nom, clf in classifieurs.items():
+            y_proba = clf.prediction_proba()
+
+          
+            roc_auc_total = 0
+
+          
+            for i in range(n_classes):
+                fpr, tpr, _ = roc_curve(y_test_binarise[:, i], y_proba[:, i])
+                roc_auc = auc(fpr, tpr)
+                roc_auc_total += roc_auc
+
+            # Calculer l'AUC moyenne pour le classifieur
+            roc_auc_moyen = roc_auc_total / n_classes
+            plt.plot(fpr, tpr, lw=2, label=f'{nom} (AUC moyen = {roc_auc_moyen:.2f})')
+
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('Taux de Faux Positifs')
+        plt.ylabel('Taux de Vrais Positifs')
+        plt.title('Courbes ROC Moyennes des différents classifieurs')
+        plt.legend(loc='lower right')
         plt.grid()
         plt.show()
